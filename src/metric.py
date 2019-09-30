c834a7ba06e584d0517c2a07993f5729c449832e
namshik <physhik@gmail.com>
2019-09-30 04:19:13 -0700

    modify to input date range

--- src/metric.py
+++ src/metric.py
@@ -4,17 +4,16 @@ from bs4 import BeautifulSoup
 import requests
 import io
 
-def get_soup(start_season, end_season, league, qual, ind):
+def get_soup(start_season, end_season, startdate, enddate, league, qual, ind):
     """
     fangraph web crawler 
     fangraph_pitching_leader.py
     modified from https://github.com/jldbc/pybaseball/blob/master/pybaseball/pitching_leaders.py
     you can change the table by modifying url format 
     """
-    
-    url = "http://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg={}&qual={}&type=c,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,-1&season={}&month=0&season1={}&ind={}&team=&rost=&age=&filter=&players=&page=1_100000"
+    url = "http://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg={}&qual={}&type=c,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,242,243,244,245,246,247,248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,-1&season={}&month=1000&season1={}&startdate={}&enddate={}&ind={}&team=&rost=&age=&filter=&players=&page=1_100000"
     #url = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg={}&qual={}&type=c,3,4,5,6,7,9,10,11,12,13,14,15&season={}&month=0&season1={}&ind={}'
-    url = url.format(league, qual, end_season, start_season, ind)
+    url = url.format(league, qual, end_season, start_season, startdate, enddate, ind)
     s=requests.get(url).content
     #print(s)
     return BeautifulSoup(s, "lxml")
@@ -71,7 +70,6 @@ def get_table(soup, ind):
                 data[col] = data[col].astype(str).str.strip('%')
                 data[col] = data[col].astype(float)/100.
         else:
-            #print(col)
             pass
 
     #convert everything except name and team to numeric
@@ -90,7 +88,7 @@ def get_table(soup, ind):
     
     return data
 
-def fangraphs_stats(start_season, end_season=None, league='all', qual=1, ind=1):
+def fangraphs_stats(start_season, end_season, start_date, end_date, league='all', qual=1, ind=1):
     """
     Get season-level pitching data from FanGraphs. 
     ARGUMENTS:
@@ -103,26 +101,23 @@ def fangraphs_stats(start_season, end_season=None, league='all', qual=1, ind=1):
     copied from https://github.com/jldbc/pybaseball/blob/master/pybaseball/pitching_leaders.py
 
     """
-    if start_season is None:
-        raise ValueError("You need to provide at least one season to collect data for. Try pitching_leaders(season) or pitching_leaders(start_season, end_season).")
-    if end_season is None:
-        end_season = start_season
-    soup = get_soup(start_season=start_season, end_season=end_season, league=league, qual=qual, ind=ind)
+    soup = get_soup(start_season,  end_season, start_date, end_date, league=league, qual=qual, ind=ind)
     table = get_table(soup, ind)
     return table
-  
-  
+    
 # brl, con tables from statcast
 
-def statcast_stats(year):
-
-  url_con ="https://baseballsavant.mlb.com/statcast_search/csv?hfPT=&hfAB=&hfBBT=fly%5C.%5C.ball%7Cpopup%7Cline%5C.%5C.drive%7Cground%5C.%5C.ball%7C&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea={}%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=&game_date_lt=&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=xwoba&player_event_sort=h_launch_speed&sort_order=asc&min_pas=0#results"
-  url_brl = "https://baseballsavant.mlb.com/statcast_search/csv?hfPT=&hfAB=&hfBBT=fly%5C.%5C.ball%7Cpopup%7Cline%5C.%5C.drive%7Cground%5C.%5C.ball%7C&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea={}%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=6%7C&game_date_gt=&game_date_lt=&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=xwoba&player_event_sort=h_launch_speed&sort_order=asc&min_pas=0#results"
-  url_con_wo_hr = "https://baseballsavant.mlb.com/statcast_search/csv?hfPT=&hfAB=single%7Cdouble%7Ctriple%7Cfield%5C.%5C.out%7Cdouble%5C.%5C.play%7Cgrounded%5C.%5C.into%5C.%5C.double%5C.%5C.play%7Cfielders%5C.%5C.choice%7Cfielders%5C.%5C.choice%5C.%5C.out%7Cforce%5C.%5C.out%7Csac%5C.%5C.bunt%7Csac%5C.%5C.bunt%5C.%5C.double%5C.%5C.play%7Csac%5C.%5C.fly%7Csac%5C.%5C.fly%5C.%5C.double%5C.%5C.play%7Ctriple%5C.%5C.play%7C&hfBBT=fly%5C.%5C.ball%7Cpopup%7Cline%5C.%5C.drive%7Cground%5C.%5C.ball%7C&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea={}%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=&game_date_lt=&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=xwoba&player_event_sort=h_launch_speed&sort_order=asc&min_pas=0#results"
+def statcast_stats(start_year, end_year, start_date, end_date):
+  seasons = ""
+  for year in range(start_year, end_year+1):
+    seasons += str(year) + "%7C"
+  url_con ="https://baseballsavant.mlb.com/statcast_search/csv?hfPT=&hfAB=&hfBBT=fly%5C.%5C.ball%7Cpopup%7Cline%5C.%5C.drive%7Cground%5C.%5C.ball%7C&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&{}&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt={}&game_date_lt={}&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=xwoba&player_event_sort=h_launch_speed&sort_order=asc&min_pas=0#results"
+  url_brl = "https://baseballsavant.mlb.com/statcast_search/csv?hfPT=&hfAB=&hfBBT=fly%5C.%5C.ball%7Cpopup%7Cline%5C.%5C.drive%7Cground%5C.%5C.ball%7C&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&{}&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=6%7C&game_date_gt={}&game_date_lt={}&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=xwoba&player_event_sort=h_launch_speed&sort_order=asc&min_pas=0#results"
+  url_con_wo_hr = "https://baseballsavant.mlb.com/statcast_search/csv?hfPT=&hfAB=single%7Cdouble%7Ctriple%7Cfield%5C.%5C.out%7Cdouble%5C.%5C.play%7Cgrounded%5C.%5C.into%5C.%5C.double%5C.%5C.play%7Cfielders%5C.%5C.choice%7Cfielders%5C.%5C.choice%5C.%5C.out%7Cforce%5C.%5C.out%7Csac%5C.%5C.bunt%7Csac%5C.%5C.bunt%5C.%5C.double%5C.%5C.play%7Csac%5C.%5C.fly%7Csac%5C.%5C.fly%5C.%5C.double%5C.%5C.play%7Ctriple%5C.%5C.play%7C&hfBBT=fly%5C.%5C.ball%7Cpopup%7Cline%5C.%5C.drive%7Cground%5C.%5C.ball%7C&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&{}&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt={}&game_date_lt={}&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=xwoba&player_event_sort=h_launch_speed&sort_order=asc&min_pas=0#results"
   
-  s_con=requests.get(url_con.format(year))
-  s_brl=requests.get(url_brl.format(year))
-  s_con_wo_hr = requests.get(url_con_wo_hr.format(year))
+  s_con=requests.get(url_con.format(seasons, start_date, end_date))
+  s_brl=requests.get(url_brl.format(seasons, start_date, end_date))
+  s_con_wo_hr = requests.get(url_con_wo_hr.format(year, start_date, end_date))
   con= pd.read_csv(io.StringIO(s_con.text))
   brl = pd.read_csv(io.StringIO(s_brl.text))
   con_wohr = pd.read_csv(io.StringIO(s_con_wo_hr.text)) 
@@ -130,12 +125,12 @@ def statcast_stats(year):
   return con, brl, con_wohr
 
 
-def new_metric(year):
+def new_metric(start_year, end_year, start_date, end_date):
   
   idmap = pd.read_csv("../assets/master.csv", encoding="ISO-8859-1") 
   
-  con, brl, con_wohr= statcast_stats(year)
-  fg  = fangraphs_stats(year)
+  con, brl, con_wohr= statcast_stats(start_year, end_year, start_date, end_date)
+  fg  = fangraphs_stats(start_year, end_year, start_date, end_date)
   con.set_index('player_id', inplace=True)
   con_wohr.set_index('player_id', inplace=True)
   brl.set_index('player_id', inplace=True)
